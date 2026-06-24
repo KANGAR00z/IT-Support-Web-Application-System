e3b44e9a0895eee258edaacd186e9863879a3647
Songpan Sae-chan <darkness7256@gmail.com>
2026-06-24 21:15:24 +0700

    Initial commit: Setup project files

---
+++ DEMO/config.js
@@ -0,0 +1,4 @@
+// ไฟล์ config.js สำหรับตั้งค่าระบบ FAST TICKET
+
+const MY_LIFF_ID = "2010392375-KFCzC8ai"; 
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbybRO1Y0TROyDJn9V2-QkbiH1BIVrDm6vJGEdW9hi_mbTF8C0CDWtaajQrOkECNOAg/exec";
